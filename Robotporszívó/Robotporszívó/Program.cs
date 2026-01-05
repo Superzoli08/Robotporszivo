b1ac0b4f90d77941bf5a477c6116ab7239f1f6a7
Superzoli08 <b3vetzol@vasvari.org>
2026-01-05 01:13:43 +0100

    Szinte kész a feladat. Térképbeállitas optimazilasa es telnegesmukodese, takaritas robotmozgasanak allitgatasa (meg mindig egy kalap szar) es amugy kb kesz niganiganiganignaingaingia

--- Robotporszívó/Robotporszívó/Program.cs
+++ Robotporszívó/Robotporszívó/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace Robotporszívó
 {
@@ -11,41 +8,20 @@ namespace Robotporszívó
         static void Main(string[] args)
         {
             Console.OutputEncoding = System.Text.Encoding.UTF8;
-            int[,] terkep = Generalas();
 
-            char beallitas = ' ';
-            while (beallitas != 'S' && beallitas != 's')
-            {
-                beallitas = Beallitasok(terkep);
-            }
-        }
-        static char Beallitasok(int[,] terkep)
-        {
-            Console.WriteLine("Robot - Beállítások:");
-            Console.WriteLine("Takarítás (C), Térkép (T), Újragenerálás (G), Program leállítása (S)");
-            Console.WriteLine("----------------------------------------------------------------------");
-            Console.Write("---------> ");
-            char beallitas = Convert.ToChar(Console.ReadLine());
-            while (beallitas != 'C' && beallitas != 'T' && beallitas != 'G' && beallitas != 'S' && beallitas != 'c' && beallitas != 't' && beallitas != 'g' && beallitas != 's')
+            int[,] terkep = Generalas();
+            char valasztas = ' ';
+
+            while (valasztas != 'S' && valasztas != 's')
             {
                 Console.Clear();
                 Console.WriteLine("Robot - Beállítások:");
-                Console.WriteLine("Takarítás (C), Térkép (T), Újragenerálás (G), Program leállítása (S)");
-                Console.WriteLine("----------------------------------------------------------------------");
+                Console.WriteLine("Takarítás (C), Térkép (T), Újragenerálás (G), Kilépés (S)");
                 Console.Write("---------> ");
-                beallitas = Convert.ToChar(Console.ReadLine());
-            }
-            Console.WriteLine();
-            if (beallitas == 'C' || beallitas == 'c')
-            {
-                Console.Clear();
-                Takaritas(terkep);
-            }
-            else if (beallitas == 'T' || beallitas == 't')
-            {
-                Console.Clear();
-                Terkep(terkep);
 
+<<<<<<< HEAD
+                valasztas = Convert.ToChar(Console.ReadLine());
+=======
             }
             else if (beallitas == 'G' || beallitas == 'g')
             {
@@ -56,123 +32,184 @@ namespace Robotporszívó
             {
                 Console.WriteLine("A program leállt.");
             }
+>>>>>>> cc1bcb0f669b3fbbbef808d1f6bf1c0808eec6ee
 
-            return beallitas;
+                while (valasztas != 'C' && valasztas != 'c' &&
+                       valasztas != 'T' && valasztas != 't' &&
+                       valasztas != 'G' && valasztas != 'g' &&
+                       valasztas != 'S' && valasztas != 's')
+                {
+                    Console.Write("Hibás választás! Újra: ");
+                    valasztas = Convert.ToChar(Console.ReadLine());
+                }
+
+                if (valasztas == 'C' || valasztas == 'c')
+                {
+                    terkep = Takaritas(terkep);
+                }
+                else if (valasztas == 'T' || valasztas == 't')
+                {
+                    TerkepKirajzol(terkep);
+                    Console.ReadKey();
+                }
+                else if (valasztas == 'G' || valasztas == 'g')
+                {
+                    terkep = Generalas();
+                }
+            }
         }
-        static int[,] Generalas() 
+
+        static int[,] Generalas()
         {
-            int[,] terkep = new int[0, 0];
+            int n = 0;
+            int m = 0;
+
+            while (n < 20 || n > 30)
+            {
+                Console.Write("Sorok száma [20-30]: ");
+                n = Convert.ToInt32(Console.ReadLine());
+            }
+
+            while (m < 20 || m > 30 || m == n)
+            {
+                Console.Write("Oszlopok száma [20-30], nem lehet egyenlő: ");
+                m = Convert.ToInt32(Console.ReadLine());
+            }
+
+            int[,] terkep = new int[n, m];
+            Random rnd = new Random();
+
             int szabad = 0;
             int koszos = 0;
-            while (szabad < 1 || koszos < 1)
+
+            while (szabad == 0 || koszos == 0)
             {
-                Console.WriteLine("-----------------------------------------------");
-                Console.WriteLine("ROBOTPORSZÍVÓ TÉRKÉPGENERÁLÁS");
-                Console.WriteLine("-----------------------------------------------");
+                szabad = 0;
+                koszos = 0;
 
-                int n = 0;
-                int m = 0;
-
-                Console.WriteLine("Adj meg a szélét a térképnek [20; 30] intervallum között: ");
-                n = Convert.ToInt32(Console.ReadLine());
-                while (n < 20 || n > 30)
+                for (int i = 0; i < n; i++)
                 {
-                    Console.WriteLine("Hiba: Add meg a paraméteren belül: [20; 30]");
-                    Console.WriteLine("Adj meg a szélét a térképnek [20; 30] intervallum között: ");
-                    n = Convert.ToInt32(Console.ReadLine());
-                }
-
-
-                Console.WriteLine("Adj meg a magasságát a térképnek [20; 30] intervallum között: ");
-                m = Convert.ToInt32(Console.ReadLine());
-                while (m < 20 || m > 30)
-                {
-                    Console.WriteLine("Hiba: Add meg a paraméteren belül: [20; 30]");
-                    Console.WriteLine("Adj meg a magasságát a térképnek [20; 30] intervallum között: ");
-                    m = Convert.ToInt32(Console.ReadLine());
-                }
-
-
-                while (n == m)
-                {
-                    Console.Clear();
-                    Console.WriteLine("------------------------------------------------");
-                    Console.WriteLine("HIBA: A SZÉLESSÉG ÉS MAGASSÁG NEM LEHET EGYENLŐ!");
-                    Console.WriteLine("------------------------------------------------");
-                    Console.ReadKey();
-                    Console.Clear();
-
-                    Console.WriteLine("Adj meg a szélét a térképnek [20; 30] intervallum között: ");
-                    n = Convert.ToInt32(Console.ReadLine());
-                    while (n < 20 || n > 30)
+                    for (int j = 0; j < m; j++)
                     {
-                        Console.WriteLine("Hiba: Add meg a paraméteren belül: [20; 30]");
-                        Console.WriteLine("Adj meg a szélét a térképnek [20; 30] intervallum között: ");
-                        n = Convert.ToInt32(Console.ReadLine());
-                    }
+                        int r = rnd.Next(100);
 
-
-                    Console.WriteLine("Adj meg a magasságát a térképnek [20; 30] intervallum között: ");
-                    m = Convert.ToInt32(Console.ReadLine());
-                    while (m < 20 || m > 30)
-                    {
-                        Console.WriteLine("Hiba: Add meg a paraméteren belül: [20; 30]");
-                        Console.WriteLine("Adj meg a magasságát a térképnek [20; 30] intervallum között: ");
-                        m = Convert.ToInt32(Console.ReadLine());
+                        if (r < 50)
+                        {
+                            terkep[i, j] = '-';
+                            szabad++;
+                        }
+                        else if (r < 70)
+                        {
+                            terkep[i, j] = 'b';
+                        }
+                        else
+                        {
+                            terkep[i, j] = 'k';
+                            koszos++;
+                        }
                     }
                 }
-
-                terkep = new int[n, m];
-                szabad = (n * m) / 100 * 50;
-                int akadaly = (n * m) / 100 * 20;
-                koszos = (n * m) / 100 * 30;
-            }
-            Console.WriteLine();
-            Console.Write("┏");
-            for (int i = 0; i < terkep.GetLength(1)*2; i++)
-            {
-                Console.Write("━");
             }
 
-            Console.Write("┓");
-            Console.WriteLine();
-            for (int i = 0; i < terkep.GetLength(0); i++)
+            bool robotLetett = false;
+            int rx = 0;
+            int ry = 0;
+
+            while (!robotLetett)
             {
-                Console.Write("┃");
-                for (int j = 0; j < terkep.GetLength(1); j++)
+                rx = rnd.Next(n);
+                ry = rnd.Next(m);
+
+                if (terkep[rx, ry] == '-')
                 {
-                    terkep[i, j] = 'p';
-                    Console.Write($"p ");
+                    terkep[rx, ry] = 'r';
+                    robotLetett = true;
                 }
-                Console.Write("┃");
-                Console.WriteLine();
-            }
-            Console.Write("┗");
-            for (int i = 0; i < terkep.GetLength(1) * 2; i++)
-            {
-                Console.Write("━");
             }
 
-            Console.Write("┛");
-            Console.WriteLine();
-            Console.ReadKey();
             Console.Clear();
+            TerkepKirajzol(terkep);
+            Console.ReadKey();
             return terkep;
         }
 
-        static void Takaritas(int[,] terkep)
+
+
+        static int[,] Takaritas(int[,] terkep)
         {
-            Console.WriteLine("A robot elkezdi a takarítást...");
-            Console.ReadKey();
-            Console.Clear();
+            int lepesek = 0;
+            int feltakaritott = 0;
+
+            int rx = 0, ry = 0;
+            int n = terkep.GetLength(0);
+            int m = terkep.GetLength(1);
+
+            // Robot pozíciójának keresése
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    if (terkep[i, j] == 'r')
+                    {
+                        rx = i;
+                        ry = j;
+                    }
+                }
+            }
+
+            // Logikus takarítás: sor és oszlop szerint végig minden koszos cellán
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    if (terkep[i, j] == 'k')
+                    {
+                        // először sor szerint közelít
+                        while (rx != i)
+                        {
+                            terkep[rx, ry] = '-';
+                            if (rx < i) rx++; else rx--;
+                            if (terkep[rx, ry] == 'k') feltakaritott++;
+                            terkep[rx, ry] = 'r';
+                            lepesek++;
+                            Console.Clear();
+                            TerkepKirajzol(terkep);
+                            Thread.Sleep(100);
+                        }
+
+                        // majd oszlop szerint
+                        while (ry != j)
+                        {
+                            terkep[rx, ry] = '-';
+                            if (ry < j) ry++; else ry--;
+                            if (terkep[rx, ry] == 'k') feltakaritott++;
+                            terkep[rx, ry] = 'r';
+                            lepesek++;
+                            Console.Clear();
+                            TerkepKirajzol(terkep);
+                            Thread.Sleep(100);
+                        }
+
+                        // takarítva
+                        terkep[rx, ry] = 'r';
+                    }
+                }
+            }
+            return terkep;
         }
-        static void Terkep(int[,] terkep)
+
+            static void TerkepKirajzol(int[,] terkep)
         {
-            Console.WriteLine("A robot térképet jelenít meg...");
             for (int i = 0; i < terkep.GetLength(0); i++)
             {
                 for (int j = 0; j < terkep.GetLength(1); j++)
                 {
+<<<<<<< HEAD
+                    Console.Write((char)terkep[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+=======
                     Console.Write($"K ");
                 }
                 Console.WriteLine();
@@ -185,6 +222,7 @@ namespace Robotporszívó
             Console.WriteLine("A robot újragenerálja a térképet...");
             terkep = Generalas();
             return terkep;
+>>>>>>> cc1bcb0f669b3fbbbef808d1f6bf1c0808eec6ee
         }
     }
 }
